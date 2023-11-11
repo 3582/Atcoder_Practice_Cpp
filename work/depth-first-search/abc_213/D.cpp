fd95a84c8f3aedf2a08e05d75a33ecc5fd3577ee
cat3582 <cstbwy@gmail.com>
2023-11-11 11:56:30 +0900

    comment

--- work/depth-first-search/abc_213/D.cpp
+++ work/depth-first-search/abc_213/D.cpp
@@ -7,8 +7,11 @@ vector<int> ans;
 
 void dfs(int crr, int pre)
 {
+    // DFS(今いる町,前にいた町)
     ans.push_back(crr);
+    // nxt=今いる町から行ける町
     for (int nxt : G[crr])
+        // もしnxtが前にいた町と違うなら、更に先へ探索する
         if (nxt != pre)
         {
             dfs(nxt, crr);
@@ -21,6 +24,8 @@ int main()
     int N;
     cin >> N;
     G.resize(N + 1);
+    // 道の情報受け取り
+    // G[2]=[1,4]ならば2と1,4がつながっている
     for (int i = 0; i < N - 1; i++)
     {
         int a, b;
@@ -28,6 +33,7 @@ int main()
         G[a].push_back(b);
         G[b].push_back(a);
     }
+    // 小さい順に回るからソート
     for (int i = 1; i <= N; i++)
         sort(G[i].begin(), G[i].end());
 
