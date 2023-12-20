cbfdc3bf428af830ec84ef26c196e835c98dcd62
cat3582 <cstbwy@gmail.com>
2023-12-20 14:27:29 +0900

    abc331_C

--- work/contest/abc_331/C.cpp
+++ work/contest/abc_331/C.cpp
@@ -1,38 +1,37 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
+
+typedef long long int ll;
 
 int main()
 {
-    int N;
-    cin >> N;
-    vector<int> A(N);
-    vector<int> result(N, 0);
-    for (int i = 0; i < N; ++i)
-        cin >> A.at(i);
-
-    for (int i = 0; i < N; ++i)
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    vector<int> b(n);
+    for (int i = 0; i < n; ++i)
     {
-        for (int j = 0; j < N; j++)
-        {
-            if (A.at(i) < A.at(j))
-            {
-                result.at(i) += A.at(j);
-            }
-        }
+        cin >> a[i];
+        b[i] = a[i];
+    }
+    // 昇順
+    sort(b.begin(), b.end());
+    vector<ll> s(n + 1);
+    // 累積和
+    s[0] = 0;
+    for (int i = 0; i < n; ++i)
+    {
+        s[i + 1] = s[i] + b[i];
     }
 
-    for (int i = 0; i < N; i++)
+    for (int i = 0; i < n; ++i)
     {
-        cout << result.at(i);
-        if (i + 1 != N)
-        {
-            cout << " ";
-        }
-        else
-        {
-            cout << endl;
-        }
+        // 二部探索
+        auto it = upper_bound(b.begin(), b.end(), a[i]);
+        // 累積和の
+        // it - b.begin() : この式はイテレータ it とベクター b の開始イテレータb.begin() の間の距離を計算します。
+        // この距離は、b 内の a[i] より大きい最初の要素のインデックスに相当します。
+        cout << s[n] - s[it - b.begin()] << " \n"[i == n - 1];
     }
 
     return 0;
