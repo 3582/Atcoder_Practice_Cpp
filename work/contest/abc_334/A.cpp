a60d7d150bdaab7a22728cfa896320a20022ac11
cat3582 <cstbwy@gmail.com>
2023-12-23 22:57:57 +0900

    abc_334

---
+++ work/contest/abc_334/A.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    int B, G;
+    cin >> B >> G;
+    if (B > G)
+    {
+        cout << "Bat" << endl;
+    }
+    else
+    {
+        cout << "Glove" << endl;
+    }
+
+    return 0;
+}
