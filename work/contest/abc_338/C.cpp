f033fc62bc0b3143576750278b5f564275e29534
cat3582 <cstbwy@gmail.com>
2024-01-27 22:43:01 +0900

    abc_338_C

--- work/contest/abc_338/C.cpp
+++ work/contest/abc_338/C.cpp
@@ -56,3 +56,47 @@ int main()
     cout << max_dishes << endl;
     return 0;
 }
+
+// #include <iostream>
+// #include <vector>
+// #include <climits>
+// #include <algorithm> // この行を追加
+
+// using namespace std;
+
+// int main() {
+//     int N;
+//     cin >> N;
+
+//     vector<int> Q(N), A(N), B(N);
+//     for (int i = 0; i < N; ++i) {
+//         cin >> Q[i];
+//     }
+//     for (int i = 0; i < N; ++i) {
+//         cin >> A[i];
+//     }
+//     for (int i = 0; i < N; ++i) {
+//         cin >> B[i];
+//     }
+
+//     const long long INF = LLONG_MAX;
+//     long long ans = 0;
+
+//     int maxQ = *max_element(Q.begin(), Q.end());
+//     for (int x = 0; x <= maxQ; ++x) {
+//         long long y = INF;
+//         for (int i = 0; i < N; ++i) {
+//             if (Q[i] < A[i] * x) {
+//                 y = -INF;
+//                 break; // この場合、ループを抜けます
+//             } else if (B[i] > 0) {
+//                 y = min(y, static_cast<long long>((Q[i] - A[i] * x) / B[i]));
+//             }
+//         }
+//         ans = max(ans, x + y);
+//     }
+
+//     cout << ans << endl;
+
+//     return 0;
+// }
